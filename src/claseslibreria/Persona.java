61b5964cb70571efecdd10ad45e6765bb86001e0
dam112 <dam112@PC2012.aulas.local>
2016-04-11 10:31:23 +0200

    commit 1

---
+++ src/claseslibreria/Persona.java
@@ -0,0 +1,15 @@
+package claseslibreria;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dam112
+ */
+public class Persona {
+    
+}
